2415a5c8e7a6709e77ebd36a6b14e63239fa2a88
agnes97 <janachaloupkova97@gmail.com>
2023-09-28 21:24:37 +0200

    🔐 Add quick movie app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,56 @@
-import styles from './page.module.css'
-import { Main } from './page.styled'
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import styles from "./page.module.css";
+import { Main } from "./page.styled";
+
+export const disenchatnmentSeason4 = [
+  // very legal video links
+];
 
 export default function Home() {
-	return (
-		<Main>
-			<div className={styles.description}>
-				<p>
-					Get started by editing&nbsp;
-					<code className={styles.code}>src/app/page.tsx</code>
-				</p>
-				<div>
-					<a
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{' '}
-						<Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-					</a>
-				</div>
-			</div>
+  const [currentEpisode, setCurrentEpisode] = useState(0);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-			<div className={styles.center}>
-				<Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-			</div>
+  const updateEpisode = () => {
+    setCurrentEpisode((prev) => prev + 1);
+  };
 
-			<div className={styles.grid}>
-				<a
-					href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Docs <span>-&gt;</span>
-					</h2>
-					<p>Find in-depth information about Next.js features and API.</p>
-				</a>
+  useEffect(() => {
+    videoRef?.current?.addEventListener("ended", updateEpisode);
+    // videoRef?.current?.requestFullscreen();
+    // videoRef?.current?.focus();
 
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Learn <span>-&gt;</span>
-					</h2>
-					<p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-				</a>
+    return () => videoRef?.current?.removeEventListener("ended", updateEpisode);
+  }, [videoRef.current]);
 
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Templates <span>-&gt;</span>
-					</h2>
-					<p>Explore the Next.js 13 playground.</p>
-				</a>
+  return (
+    <Main>
+      <div className={styles.description}>
+        <div>Disenchantment</div>
+      </div>
 
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Deploy <span>-&gt;</span>
-					</h2>
-					<p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-				</a>
-			</div>
-		</Main>
-	)
+      <div
+        className={styles.center}
+        style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
+      >
+        <div style={{ display: "flex", justifyContent: "center", gap: "2rem" }}>
+          {currentEpisode + 1}
+          <button onClick={updateEpisode}>NEXT EPISODE</button>
+        </div>
+        <video
+          controls
+          autoPlay={currentEpisode > 0}
+          key={disenchatnmentSeason4[currentEpisode]}
+          ref={videoRef}
+        >
+          <h2>Episode {disenchatnmentSeason4[currentEpisode + 1]}</h2>
+          <source
+            src={disenchatnmentSeason4[currentEpisode]}
+            type="video/webm"
+          />
+        </video>
+      </div>
+    </Main>
+  );
 }
