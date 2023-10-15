976c00eb9224dfc69ae17c9a115cc178d18866f5
agnes97 <janachaloupkova97@gmail.com>
2023-10-15 13:44:16 +0200

    ✨ Smooth animation for GalleryCard

--- src/app/components/gallery-card/GalleryCard.tsx
+++ src/app/components/gallery-card/GalleryCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FC, MouseEvent, useEffect, useRef, useState } from "react";
+import React, { FC, MouseEvent, useRef, useState } from "react";
 import { BackgroundImage, Card, Content, Shadow } from "./GalleryCard.styled";
 
 type MousePosition = {
@@ -15,18 +15,12 @@ const GalleryCard: FC = () => {
   const [mousePosition, setMousePosition] =
     useState<MousePosition>(initialMousePosition);
 
-  const cardRef = useRef<HTMLDivElement>(null);
-  const card = cardRef.current;
-
-  const lerp = (start: number, end: number, amount: number) => {
-    return (1 - amount) * start + amount * end;
-  };
-
   const remap = (value: number, oldMax: number, newMax: number) => {
     const newValue = ((value + oldMax) * (newMax * 2)) / (oldMax * 2) - newMax;
     return Math.min(Math.max(newValue, -newMax), newMax);
   };
 
+  const cardRef = useRef<HTMLDivElement>(null);
   const handleMouseMove = (event: MouseEvent) => {
     const card = cardRef.current;
     if (!card) return;
@@ -35,32 +29,14 @@ const GalleryCard: FC = () => {
     const rect = card.getBoundingClientRect();
     const centerX = (rect.left + rect.right) / 2;
     const centerY = (rect.top + rect.bottom) / 2;
-    const posX = event.pageX - centerX;
-    const posY = event.pageY - centerY;
+    const positionY = event.pageX - centerX;
+    const positionX = event.pageY - centerY;
 
     setMousePosition({
-      x: remap(posX, rect.width / 2, maxRotationAngle),
-      y: -remap(posY, rect.height / 2, maxRotationAngle),
+      y: remap(positionY, rect.width / 2, maxRotationAngle),
+      x: -remap(positionX, rect.height / 2, maxRotationAngle),
     });
   };
-
-  useEffect(() => {
-    if (!card) return;
-
-    let currentX = parseFloat(
-      card.style.getPropertyValue("--rotateY").slice(0, -1)
-    );
-    let currentY = parseFloat(
-      card.style.getPropertyValue("--rotateX").slice(0, -1)
-    );
-    if (isNaN(currentX)) currentX = 0;
-    if (isNaN(currentY)) currentY = 0;
-
-    const x = lerp(currentX, mousePosition.x, 0.05);
-    const y = lerp(currentY, mousePosition.y, 0.05);
-
-    setMousePosition({ x, y });
-  }, []);
 
   return (
     <Card
@@ -69,25 +45,25 @@ const GalleryCard: FC = () => {
         setMousePosition(initialMousePosition);
       }}
       onMouseMove={(event) => handleMouseMove(event)}
-      data-rotate-x={mousePosition.y}
-      data-rotate-y={mousePosition.x}
+      data-rotate-x={mousePosition.x}
+      data-rotate-y={mousePosition.y}
       borderColors={["#dd7db7", "#87c8fd"]}
       transparentBorderSides={["left"]}
     >
-      <Shadow data-rotate-x={mousePosition.y} data-rotate-y={mousePosition.x} />
+      <Shadow data-rotate-x={mousePosition.x} data-rotate-y={mousePosition.y} />
       <BackgroundImage
         data-type="background"
-        data-rotate-x={mousePosition.y}
-        data-rotate-y={mousePosition.x}
+        data-rotate-x={mousePosition.x}
+        data-rotate-y={mousePosition.y}
       />
       <BackgroundImage
         data-type="cutout"
-        data-rotate-x={mousePosition.y}
-        data-rotate-y={mousePosition.x}
+        data-rotate-x={mousePosition.x}
+        data-rotate-y={mousePosition.y}
       />
       <Content
-        data-rotate-x={mousePosition.y}
-        data-rotate-y={mousePosition.x}
+        data-rotate-x={mousePosition.x}
+        data-rotate-y={mousePosition.y}
       />
     </Card>
   );
