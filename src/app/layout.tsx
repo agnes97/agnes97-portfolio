e33c06f891a9e8ff07242758d90479400bff2ed2
agnes97 <janachaloupkova97@gmail.com>
2023-11-10 14:16:39 +0100

    ✨ Add styled-components Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import StyledComponentsRegistry from "@/lib/styled-component-registry";
 import { Inter } from "next/font/google";
 import React, { ReactNode } from "react";
-import { GlobalStyle } from "./styles/global.styled";
+import StyledComponentsProvider from "./providers/styled-components-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <GlobalStyle />
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </body>
+      <StyledComponentsProvider>{children}</StyledComponentsProvider>
     </html>
   );
 }
