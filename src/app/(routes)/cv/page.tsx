9c57043dce1e8e5ac4639edac48b055906f0e36b
agnes97 <janachaloupkova97@gmail.com>
2023-11-11 18:31:35 +0100

    ✨ Prepare structure for CV page

--- src/app/(routes)/cv/page.tsx
+++ src/app/(routes)/cv/page.tsx
@@ -1,5 +1,44 @@
+"use client";
+
+import { Divider } from "@/app/styles/global.styled";
 import React from "react";
 
 export default function Cv() {
-  return <div>HELLO CV</div>;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10rem",
+        }}
+      >
+        Header
+      </div>
+      <Divider />
+      <div
+        style={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10rem",
+        }}
+      >
+        Secret Info
+      </div>
+      <Divider />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10rem",
+        }}
+      >
+        Footer
+      </div>
+    </>
+  );
 }
