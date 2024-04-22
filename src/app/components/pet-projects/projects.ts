d566e287c9dd391154a9b7ff6e2b69849e0c16e5
agnes97 <jana@macovi.space>
2024-04-22 22:17:48 +0200

    🍱 Update projects data

--- src/app/components/pet-projects/projects.ts
+++ src/app/components/pet-projects/projects.ts
@@ -1,32 +1,56 @@
 import { Project } from '../project-card/ProjectCard';
 
-import agnes_light_cutout from '../../assets/agnes-light-cutout.png';
+import birthday_boi from '../../assets/projects/birthday-boi.png';
+import budget_budgie_cutout from '../../assets/projects/budget-budgie.png';
+import portfolio from '../../assets/projects/portfolio.png';
+import prismify from '../../assets/projects/prismify.png';
+import wedding from '../../assets/projects/wedding.jpg';
 
 export const projects: Project[] = [
   {
-    id: 'birthday_boi',
-    title: 'Birthday Boi',
-    description: 'Popis',
-    thumbnailSrc: agnes_light_cutout.src,
-    vimeoVideoId: '936767946',
-  },
-  {
-    id: 'wedding',
-    title: '<Wedding>',
-    description: 'Popis',
-    thumbnailSrc: agnes_light_cutout.src,
+    id: 'portfolio',
+    title: 'Portfolio',
+    description: '~ this project ~',
+    githubRepoUrl: 'https://github.com/agnes97/agnes97-portfolio',
+    thumbnailSrc: portfolio.src,
+    isThumbnailTransparent: true,
   },
   {
     id: 'prismify',
     title: 'Prismify',
-    description: 'Popis',
-    thumbnailSrc: agnes_light_cutout.src,
+    description:
+      'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit.',
+    githubRepoUrl: 'https://github.com/agnes97/prismify',
+    url: 'https://prismify.macovi.space',
+    thumbnailSrc: prismify.src,
   },
-
   {
     id: 'budget_budgie',
     title: 'Budget Budgie',
-    description: 'Popis',
-    thumbnailSrc: agnes_light_cutout.src,
+    description: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit.',
+    githubRepoUrl: 'https://github.com/agnes97/budget-budgie',
+    url: 'https://budget-budgie.netlify.app/',
+    thumbnailSrc: budget_budgie_cutout.src,
+    isThumbnailTransparent: true,
+  },
+  {
+    id: 'birthday_boi',
+    title: 'Birthday Boi',
+    description:
+      'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet.',
+    githubRepoUrl: 'https://github.com/agnes97/birthday-boi',
+    url: 'https://birthday-boi.netlify.app/',
+    thumbnailSrc: birthday_boi.src,
+    vimeoVideoId: '936767946',
+    backBackgroundColor: '#2b2b2b',
+  },
+  {
+    id: 'wedding',
+    title: '<Wedding>',
+    description:
+      'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Lorem Ipsum Dolor Sit Amet.',
+    githubRepoUrl: 'https://github.com/out-fox-it/wedding',
+    url: 'https://wedding.macovi.space/',
+    thumbnailSrc: wedding.src,
   },
 ];
