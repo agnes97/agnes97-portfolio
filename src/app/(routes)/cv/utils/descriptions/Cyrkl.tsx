c7023ecc3ea28a54e61986a4814e53b88135beec
agnes97 <jana@macovi.space>
2024-05-25 21:24:15 +0200

    ♻️ Update Cyrkl description

--- src/app/(routes)/cv/utils/descriptions/Cyrkl.tsx
+++ src/app/(routes)/cv/utils/descriptions/Cyrkl.tsx
@@ -1,10 +1,8 @@
 import Flex from '@/app/components/flex/Flex';
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 
 import { DescriptionProps } from '../get-company-description';
 import {
-  AdditionalAnimatedImages,
-  AnimationButton,
   DescriptionList,
   TechStackTitle,
 } from '../../components/experiences/Experiences.styled';
@@ -32,6 +30,7 @@ import cyrkl18 from '@/app/assets/cv/cyrkl/cyrkl18.png';
 import cyrkl19 from '@/app/assets/cv/cyrkl/cyrkl19.jpg';
 import cyrkl20 from '@/app/assets/cv/cyrkl/cyrkl20.jpg';
 import cyrkl21 from '@/app/assets/cv/cyrkl/cyrkl21.jpg';
+import cyrkl22 from '@/app/assets/cv/cyrkl/cyrkl22.jpg';
 
 const imagesFirstPart = [cyrkl1, cyrkl2, cyrkl3, cyrkl4];
 
@@ -53,90 +52,59 @@ const images = [
   cyrkl19,
   cyrkl20,
   cyrkl21,
+  cyrkl22,
 ];
 
-const Cyrkl: FC<DescriptionProps> = ({ type, tech_stack }) => {
-  const [areAllImagesVisible, setAreAllImagesVisible] = useState(false);
+const Cyrkl: FC<DescriptionProps> = ({ type, tech_stack }) => (
+  <Flex flexDirection='column' gap='1rem'>
+    <p>
+      At this start-up, the work moved at the speed of light, and my growth from
+      a very green junior to a confident frontend developer was just as swift. I
+      was able to enhance both my soft and hard skills, from refactoring old
+      code to introducing new technologies to the codebase, such as switching to
+      App Router. I worked as a bug validator to catch the most elusive bugs,
+      served as a scrum master leading meetings for my team, and much more. I've
+      even briefly sticked my head to the (Google) cloud. I had the freedom to
+      innovate, balanced by full responsibility for my features, from writing
+      the issue to deployment.
+    </p>
 
-  return (
-    <Flex flexDirection='column' gap='1rem'>
-      <p>
-        At this start-up, the work moved at the speed of light, and my growth
-        from a very green junior to a confident frontend developer was just as
-        swift. I was able to enhance both my soft and hard skills, from
-        refactoring old code to introducing new technologies to the codebase,
-        such as switching to App Router. I worked as a bug validator to catch
-        the most elusive bugs, served as a scrum master leading meetings for my
-        team, and much more. I've even briefly sticked my head to the (Google)
-        cloud. I had the freedom to innovate, balanced by full responsibility
-        for my features, from writing the issue to deployment.
-      </p>
+    <p>
+      During my time here, I was proud of the playful titles my colleagues gave
+      me. Some were official, others inside jokes—I'll let you decipher which is
+      which. Things might not be as they seem!
+    </p>
 
-      <p>
-        During my time here, I was proud of the playful titles my colleagues
-        gave me. Some were official, others inside jokes—I'll let you decipher
-        which is which. Things might not be as they seem!
-      </p>
+    <DescriptionList>
+      <li>a rescue ranger</li>
+      <li>chief quality officer</li>
+      <li>component comittee member</li>
+      <li>front-end guidlines owner</li>
+      <li>dev conf organiser</li>
+    </DescriptionList>
 
-      <DescriptionList>
-        <li>a rescue ranger</li>
-        <li>chief quality officer</li>
-        <li>component comittee member</li>
-        <li>front-end guidlines owner</li>
-        <li>dev conf organiser</li>
-      </DescriptionList>
+    <p>
+      While searching for a product market fit, ever-changing requirements and
+      having an accomplished in-house designer made for a good challenge. How to
+      do the best work in the fastes way and how to write code that's as
+      future-proof as possible? Despite the rapid pace, I've took time to make
+      the developer experience better by refactoring types, writing
+      documentation or creating a PR template. We've fondly termed any such
+      activity as an "AWF" ("after work feature"). I also organised Cyrkl's
+      first internal dev conference "Dev Conf 0".
+    </p>
 
-      <p>
-        While searching for a product market fit, ever-changing requirements and
-        having an accomplished in-house designer made for a good challenge. How
-        to do the best work in the fastes way and how to write code that's as
-        future-proof as possible? Despite the rapid pace, I've took time to make
-        the developer experience better by refactoring types, writing
-        documentation or creating a PR template. We've fondly termed any such
-        activity as an "AWF" ("after work feature"). I also organised Cyrkl's
-        first internal dev conference "Dev Conf 0".
-      </p>
+    {type === 'programming' && tech_stack && (
+      <Flex alignItems='center' gap='1rem' flexWrap='wrap'>
+        <TechStackTitle>tech stack & tools:</TechStackTitle>
+        <TechStack techStack={tech_stack} />
+      </Flex>
+    )}
 
-      {type === 'programming' && tech_stack && (
-        <Flex alignItems='center' gap='1rem' flexWrap='wrap'>
-          <TechStackTitle>tech stack & tools:</TechStackTitle>
-          <TechStack techStack={tech_stack} />
-        </Flex>
-      )}
-
-      <div>
-        <DescriptionImages images={imagesFirstPart} />
-
-        {!areAllImagesVisible && (
-          <Flex justifyContent='center' alignItems='center'>
-            <AnimationButton
-              onClick={() => {
-                setAreAllImagesVisible(true);
-              }}
-            >
-              show more photos
-            </AnimationButton>
-          </Flex>
-        )}
-
-        <AdditionalAnimatedImages isAnimated={areAllImagesVisible}>
-          <div style={{ overflow: 'hidden' }}>
-            <DescriptionImages images={images} />
-
-            <Flex justifyContent='center' alignItems='center'>
-              <AnimationButton
-                onClick={() => {
-                  setAreAllImagesVisible(false);
-                }}
-              >
-                oh no, that's too many, show less!
-              </AnimationButton>
-            </Flex>
-          </div>
-        </AdditionalAnimatedImages>
-      </div>
-    </Flex>
-  );
-};
+    <div>
+      <DescriptionImages previewImages={imagesFirstPart} images={images} />
+    </div>
+  </Flex>
+);
 
 export default Cyrkl;
