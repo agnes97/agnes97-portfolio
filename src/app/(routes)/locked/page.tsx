63e28597e1a415b970da0c6a599640a1397020e6
agnes97 <jana@macovi.space>
2024-05-25 21:36:36 +0200

    🐛 Fix linting error with unused imports

--- src/app/(routes)/locked/page.tsx
+++ src/app/(routes)/locked/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Divider } from '@/app/styles/global.styled';
+// import { Divider } from '@/app/styles/global.styled';
 import React from 'react';
-import SecretContent from './components/secret-content/SecretContent';
+// import SecretContent from './components/secret-content/SecretContent';
 import Auth from './components/auth/Auth';
 
 export default function Locked() {
