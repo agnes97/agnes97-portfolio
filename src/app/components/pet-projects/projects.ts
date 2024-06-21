5c79d9bb6aaa005a375f4f43ef4cbbd05405d0d9
agnes97 <jana@macovi.space>
2024-06-21 20:16:29 +0200

    ✨ Add Czechitas Advanced Typescript pet project

--- src/app/components/pet-projects/projects.ts
+++ src/app/components/pet-projects/projects.ts
@@ -5,6 +5,7 @@ import budget_budgie_cutout from '../../assets/projects/budget-budgie.webp';
 import portfolio from '../../assets/projects/portfolio.webp';
 import prismify from '../../assets/projects/prismify.webp';
 import wedding from '../../assets/projects/wedding.webp';
+import czechitas_advanced_typescript from '../../assets/projects/czechitas-advanced-typescript.webp';
 
 export const projects: Project[] = [
   {
@@ -28,6 +29,28 @@ export const projects: Project[] = [
     thumbnailSrc: prismify.src,
   },
   {
+    id: 'wedding',
+    title: '<Wedding>',
+    description:
+      'We first build this project with wedding invitation in mind but currently it just points to a gallery and video evidence of our vows. :)',
+    techStack: ['react', 'nextjs', 'typescript', 'tailwindcss', 'vercel'],
+    githubRepoUrl: 'https://github.com/out-fox-it/wedding',
+    url: 'https://wedding.macovi.space/',
+    thumbnailSrc: wedding.src,
+  },
+  {
+    id: 'birthday_boi',
+    title: 'Birthday Boi',
+    description:
+      'I made this React game in December 2021, as a birthday present for my wonderful husband. The goal is simple - to jump over all the birthday candles rushing towards you.',
+    techStack: ['typescript', 'react', 'styled-components', 'netlify'],
+    githubRepoUrl: 'https://github.com/agnes97/birthday-boi',
+    url: 'https://birthday-boi.netlify.app/',
+    thumbnailSrc: birthday_boi.src,
+    vimeoVideoId: '936767946',
+    backBackgroundColor: '#2b2b2b',
+  },
+  {
     id: 'budget_budgie',
     title: 'Budget Budgie',
     description:
@@ -45,25 +68,13 @@ export const projects: Project[] = [
     isThumbnailTransparent: true,
   },
   {
-    id: 'birthday_boi',
-    title: 'Birthday Boi',
+    id: 'czechitas_advanced_typescript',
+    title: 'Advanced Typescript',
     description:
-      'I made this React game in December 2021, as a birthday present for my wonderful husband. The goal is simple - to jump over all the birthday candles rushing towards you.',
-    techStack: ['typescript', 'react', 'styled-components', 'netlify'],
-    githubRepoUrl: 'https://github.com/agnes97/birthday-boi',
-    url: 'https://birthday-boi.netlify.app/',
-    thumbnailSrc: birthday_boi.src,
-    vimeoVideoId: '936767946',
-    backBackgroundColor: '#2b2b2b',
-  },
-  {
-    id: 'wedding',
-    title: '<Wedding>',
-    description:
-      'We first build this project with wedding invitation in mind but currently it just points to a gallery and video evidence of our vows. :)',
-    techStack: ['react', 'nextjs', 'typescript', 'tailwindcss', 'vercel'],
-    githubRepoUrl: 'https://github.com/out-fox-it/wedding',
-    url: 'https://wedding.macovi.space/',
-    thumbnailSrc: wedding.src,
+      'Lesson for Czechitas and their "React 2" course. I\'ve decided on the subject, made the materials in a form of this project, and then taught it during the course in June 2024.',
+    techStack: ['javascript', 'typescript', 'react', 'html', 'css', 'vercel'],
+    githubRepoUrl: 'https://github.com/agnes97/czechitas-advanced-typescript',
+    url: 'https://czechitas-advanced-typescript.vercel.app/',
+    thumbnailSrc: czechitas_advanced_typescript.src,
   },
 ];
